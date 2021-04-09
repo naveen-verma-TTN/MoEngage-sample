1fb3edc1d0adbcdc2a8b42866cc91b7cf89d1dff
naveen.verma <naveen.verma@tothenew.com>
2021-04-09 21:19:53 +0530

    first commit

---
+++ app/src/main/java/com/example/moengage_sample/App.kt
@@ -0,0 +1,31 @@
+package com.example.moengage_sample
+
+import android.app.Application
+import com.moe.pushlibrary.MoEHelper
+import com.moengage.core.LogLevel
+import com.moengage.core.MoEngage
+import com.moengage.core.config.LogConfig
+import com.moengage.core.model.AppStatus
+import java.util.*
+
+
+/**
+ * Created by Naveen Verma on 9/4/21.
+ * To The New
+ * naveen.verma@tothenew.com
+ */
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val moEngage: MoEngage = MoEngage.Builder(
+            this, getString(R.string.mongege_key))
+            .configureLogs(LogConfig(LogLevel.VERBOSE, true))
+            .build()
+        MoEngage.initialise(moEngage)
+
+        MoEHelper.getInstance(applicationContext).setAppStatus(AppStatus.INSTALL)
+    }
+}
---
+++ app/src/main/java/com/example/moengage_sample/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.moengage_sample
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import com.moe.pushlibrary.MoEHelper
+import com.moe.pushlibrary.models.GeoLocation
+import com.moengage.core.Properties
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private val TAG: String = MainActivity::class.java.simpleName
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun fetchAdvertisingId(){
+        val idInfo: AdvertisingIdClient.Info? = null
+         CoroutineScope(Dispatchers.IO).launch {
+            val result = async {
+                return@async AdvertisingIdClient.getAdvertisingIdInfo(this@MainActivity)
+            }.await()
+
+             // adding GoogleAdd-Id (Unique-id)
+             val googleAddId = result.id
+             Log.i(TAG, "fetchAdvertisingId: $googleAddId")
+             MoEHelper.getInstance(applicationContext).setUniqueId(googleAddId)
+        }
+
+    }
+
+    fun trackEvents(view: View) {
+        val properties = Properties()
+        properties
+            .addAttribute("id", 2)
+            .addAttribute("name", "naveen")
+            .addAttribute("date_of_registration", java.util.Date())
+            .addAttribute("user_Location", GeoLocation(40.77, 73.98))
+        MoEHelper.getInstance(applicationContext).trackEvent("User-Details", properties)
+
+        makeToast(this, "Event track successfully")
+    }
+
+    fun loginTrack(view: View) {
+        fetchAdvertisingId()
+
+        makeToast(this, "Login successfully")
+    }
+    fun logoutTrack(view: View) {
+        MoEHelper.getInstance(applicationContext).logoutUser()
+
+        makeToast(this, "Logout successfully")
+    }
+}
+
+private fun makeToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
